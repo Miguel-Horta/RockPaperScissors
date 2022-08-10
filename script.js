2f27b7cd712040d05b6f30861df783ad24d7aae0
Miguel-Horta <miguelattero@hotmail.com>
2022-08-10 18:42:58 -0500

    Corrected de method getMessageFromResult and now the variable index is needed no more git push origin main ls exit q

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function getComputerChoice(){
     return options[choice]
 }
 
-let index = "tie"
 let playerRoundsWon = 0
 let compRoundsWon = 0
 
@@ -19,46 +18,43 @@ function playRound (playerSelection, computerSelection){
     if (fixedPlayerSelection === "rock" && computerSelection === "Rock" ||
         fixedPlayerSelection === "paper" && computerSelection === "Paper" ||
         fixedPlayerSelection === "scissors" && computerSelection === "Scissors"){
-        const tie = `It's a tie, you both chose ${fixedPlayerSelection}`
-        index = "tie"
-        return tie 
+        return 'tie'
     }
     if (fixedPlayerSelection === "rock"){
         if (computerSelection === "Paper"){
-            loser = "You lose, paper beats rock"
-            index = "lose"
-            return loser
+            return 'lose'
         }
         else if(computerSelection === "Scissors"){
-            winner = "You win! rock beats scissors"
-            index = "win"
-            return winner
+            return 'win'
         }
     }
     if (fixedPlayerSelection === "paper"){
         if (computerSelection === "Scissors"){
-            loser = "You lose, scissors cut paper"
-            index = "lose"
-            return loser
+            return 'lose'
         }
         else if(computerSelection === "Rock"){
-            winner = "You win! paper beats rock"
-            index = "win"
-            return winner
+            return 'win'
         }
     }
     if (fixedPlayerSelection === "scissors"){
         if (computerSelection === "Rock"){
-            loser = "You lose, rock beats scissors"
-            index = "lose"
-            return loser
+            return 'lose'
         }
         else if(computerSelection === "Paper"){
-            winner = "You win! scissors cut paper"
-            index = "win"
-            return winner
+            return 'win'
         }
     }
+}
+
+function getMessageFromResult(result, playerChoice, compChoice){
+    if (result === "tie") {
+        return `You tied, both chose ${playerChoice}`
+    } else if (result === "lose") {
+        return `You lose, ${compChoice} beats ${playerChoice}`
+    } else if (result === "win") {
+        return `You win, ${playerChoice} beats ${compChoice}`
+    }
+    return 'Invalid result'
 }
 
 function game(){
@@ -66,17 +62,17 @@ function game(){
         const compChoice = getComputerChoice()
         const playerinput = prompt("Write rock, paper or scissors")
         const result = playRound(playerinput,compChoice)
-        const message = getMessageFromResult(result)
+        const message = getMessageFromResult(result, playerinput, compChoice)
         console.log(message)
         
         
-        if(index === "win"){
+        if(result === "win"){
             playerRoundsWon++
         }
-        else if (index === "lose"){
+        else if (result === "lose"){
             compRoundsWon++
         }
-        else if(index === "tie"){
+        else if(result === "tie"){
             i--
         }
         console.log(`Your points:  ${playerRoundsWon} `)
@@ -89,10 +85,6 @@ function game(){
         alert("You lost :(")
     }
 
-}
-
-function getMessageFromResult(result){
-    return result
 }
 
 const play = game();
